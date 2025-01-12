c37697c6095788ce6faa34f51b0db8de97a7788b
kevinBRC <kevin.dix@brc-solar.de>
2025-01-12 17:55:40 +0100

    [kd/Bodenstation] - Finished readUserInput - start of inner class RoverManager - make construction of handleUserInput

--- ExoplanetProject/src/exoplanet/Bodenstation.java
+++ ExoplanetProject/src/exoplanet/Bodenstation.java
@@ -1,8 +1,6 @@
 package exoplanet;
-
-import exoplanet.DatabaseManager;
-import org.json.*;
 import java.io.*;
+import java.net.*;
 
 public class Bodenstation {
 	private DatabaseManager dm;
@@ -11,15 +9,53 @@ public class Bodenstation {
 	private int port;
 	private String databaseAddress;
 	private boolean hasNotification;
-	private synchronized JSONOnject buffer;
-	private synchronized JSONArray notifications;
+	//private synchronized JSONOnject buffer;
+	//private synchronized JSONArray notifications;
 
 	class RoverManager extends Thread{
+		private int latestRoverNumber;
+		private PrintWriter writeInput;
+		private BufferedReader readInput;
+		private Socket client;
+		//private JSONArray roverAlive;
+
+        public RoverManager() 
+		{}
+
+		public boolean connectToServer()
+		{
+			try 
+			{
+				this.client = new Socket(serverAddress, port);
+				this.writeInput = new PrintWriter(client.getOutputStream());
+				this.readInput = new BufferedReader(new InputStreamReader(client.getInputStream()));
+				return true;
+			} 
+			catch (IOException ex) 
+			{
+				return false;
+			}
+		}
+
+		public void run(){}
+
+		//TODO makes a rover via Server but no start yet
+		public boolean createRover()
+		{
+		}
+
+		//TODO sends command to server to deploy a rover
+		public void deployRover(JSONObject command)
+		{
+			// TODO check if rover exists in roverAlive
+		}
+
+		
 
 	}
 
 	// TODO: parameters for DatabaseManager and RoverManager
-	Bodenstation(String serverAddress, int port, String databaseAddress)
+	public Bodenstation(String serverAddress, int port, String databaseAddress)
 	{
 		this.serverAddress = serverAddress;
 		this.port = port;
@@ -33,7 +69,44 @@ public class Bodenstation {
 	// TODO: programm this method
 	private void handelUserInput(String input)
 	{
-		System.out.println("Befehl Eingegeben.");
+		switch (input) {
+			case "move":
+			case "MOVE":
+
+				break;
+			case "land":
+			case "LAND":
+				break;
+			
+			case "scan":
+			case "SCAN":
+				break;
+			
+			case "mvscan":
+			case "MVSCAN":
+				break;
+			
+			case "rotate":
+			case "ROTATE":
+				break;
+			
+			case "exit":
+			case "EXIT":
+				break;
+
+			case "getpos":
+			case "GETPOS":
+				break;
+
+			case "charge":
+			case "CHARGE":
+				break;
+			default:
+				/*Should do nothing*/
+				break;
+
+		}
+
 	}
 
 	// TODO programm this method
@@ -43,8 +116,9 @@ public class Bodenstation {
 	}
 
 
-	public String readUserInput() 
+	public void readUserInput() 
 	{
+		boolean cmdFound = false;
 		String[] possibleCommands = {"land", "scan", "move", "mvscan", "rotate", "exit", "getpos", "charge"};
 		try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)))
 		{
@@ -52,7 +126,7 @@ public class Bodenstation {
 			String input;
 			while ((input = reader.readLine()) != null) 
 			{
-                if ("exit".equalsIgnoreCase(input)) 
+                if ("disconnect".equalsIgnoreCase(input)) 
 				{
                     System.out.println("Programm beendet.");
                     break;
@@ -63,27 +137,36 @@ public class Bodenstation {
 					if(command.equals(input) || command.toUpperCase().equals(input))
 					{
 						handelUserInput(input);
-					}
-					else
-					{
-						System.out.println("Ungueltiger Befehl!");
+						cmdFound = true;
+						break;
 					}
 				}
-                System.out.println("Die Bodenstation ist bereit für den nächsten Befehl: " + input);
+				if(!cmdFound)
+				{
+					System.out.println("Ungueltiger Befehl!");
+				}
+                System.out.println("Die Bodenstation ist bereit für den nächsten Befehl: ");
 			} 
 		
 		
 		}   
 		catch (IOException ex)
 		{
-
+			System.out.println("Fehler! Konsole konnte nicht eingelesen werden!");
 		}
+	}
 
+	public int getCharge(int roverNumber)
+	{
+		
+	}
 
 	
 
 	
 	public static void main(String[] args) {
+		Bodenstation bs = new Bodenstation(null, 0, null);
+		bs.readUserInput();
 		
 	}
 
